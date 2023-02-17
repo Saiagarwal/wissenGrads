08ddcab3b4c9784872cf72987a18678769df6531
SAI AGARWAL <87272560+Saiagarwal@users.noreply.github.com>
2023-02-17 10:01:59 +0530

    Update one.java

--- one.java
+++ one.java
@@ -2,6 +2,6 @@ class One
 {
   public static void main(String args[])
   {
-    System.ouy.println("Hello world from github for the first time");
+    System.out.println("Hello world from github for the first time");
   }
 }
